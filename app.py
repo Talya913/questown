7bbd7d40647f2a2d26d2aad047b8019d24a18bbf
SynergyFam <addmitriev_1@edu.hse.ru>
2018-11-22 23:44:45 +0300

    .

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('page1.html')
 
-#@app.route('/user/<username>')
-#def username(username):
-    #user_data =
-    #return render_template('page 2.html', user=user_data)
+@app.route('/user/<username>')
+def username(username):
+    user_data = username
+    return render_template('page 2.html', user=user_data)
 
 
 app.run()
---
+++ us.py
@@ -0,0 +1,29 @@
+_users = {
+    'luap': {
+        'name': 'Luap',
+        'gender': 'f',
+        'city': 'Saint-Petersburg',
+        'preferences': 'liquor, party'
+    },
+    'rec': {
+        'name': 'Rec',
+        'gender': 'm',
+        'city': 'Moscow',
+        'preferences': 'brandy, party'
+    },
+    'busya': {
+        'name': 'Busya',
+        'gender': 'm',
+        'city': 'Saint-Petersburg',
+        'preferences': 'Sedova, water'
+    },
+    'juja': {
+        'name': 'Juja',
+        'gender': 'f',
+        'city': 'Saint-Petersburg',
+        'preferences': 'Sedova, toys'
+    }
+}
+
+def get_user(username):
+    return _users.get(username)
