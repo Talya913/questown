cc1c248c6d6b869f72db0b020a693d8c66879db1
SynergyFam <addmitriev_1@edu.hse.ru>
2018-11-23 16:24:29 +0300

    .

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import us
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 
@@ -7,10 +9,16 @@ app = Flask(__name__)
 def hello_world():
     return render_template('page1.html')
 
-@app.route('/user/<username>')
-def username(username):
-    user_data = username
-    return render_template('page 2.html', user=user_data)
+@app.route('/search')
+def search_for_person():
+    q = request.args.get('query')
+    users = us.get_users_by_name(q)
+    return render_template("search_results.html", q=q, users=users)
+
+#@app.route('/user/<username>')
+#def username(username):
+    #user_data = username
+    #return render_template('page 2.html', user=user_data)
 
 
 app.run()
--- us.py
+++ us.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 _users = {
     'luap': {
         'name': 'Luap',
@@ -24,3 +26,22 @@ _users = {
         'preferences': 'Sedova, toys'
     }
 }
+
+_user_list = []
+
+for login, user_data in _users.items():
+    _new_element = {'login': login}
+    _new_element.update(user_data)
+    _user_list.append(_new_element)
+
+
+def get_users_by_name(q):
+    results = []
+    for user in _user_list:
+        if q.lower() in user['name'].lower():
+            results.append(user)
+    return results
+
+
+def get_user(username):
+    return _users.get(username)
